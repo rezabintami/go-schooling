71dcd80ccb87ac9b7de2eb7cfbd20b7ef5fc04b9
Muhammad Reza Bintami <rezabintami@gmail.com>
2021-11-25 21:39:25 +0700

    implement http articles

--- controllers/articles/http.go
+++ controllers/articles/http.go
@@ -1,1 +1,81 @@
 package articles
+
+import (
+	"go-schooling/business/articles"
+	"go-schooling/controllers/articles/request"
+	"go-schooling/controllers/articles/response"
+	"net/http"
+	"strconv"
+
+	base_response "go-schooling/helper/response"
+
+	echo "github.com/labstack/echo/v4"
+)
+
+type ArticleController struct {
+	articleUsecase articles.Usecase
+}
+
+func NewArticleController(au articles.Usecase) *ArticleController {
+	return &ArticleController{
+		articleUsecase: au,
+	}
+}
+
+func (controller *ArticleController) Store(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	req := request.Articles{}
+	if err := c.Bind(&req); err != nil {
+		return base_response.NewErrorResponse(c, http.StatusBadRequest, err)
+	}
+
+	err := controller.articleUsecase.Store(ctx, req.ToDomain())
+	if err != nil {
+		return base_response.NewErrorResponse(c, http.StatusBadRequest, err)
+	}
+
+	return base_response.NewSuccessInsertResponse(c, "Successfully inserted")
+}
+
+func (controller *ArticleController) GetByTitle(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	title := c.QueryParam("title")
+
+	articles, err := controller.articleUsecase.GetByTitle(ctx, title)
+	if err != nil {
+		return base_response.NewErrorResponse(c, http.StatusBadRequest, err)
+	}
+
+	return base_response.NewSuccessResponse(c, response.FromDomain(articles))
+}
+
+func (controller *ArticleController) GetByID(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	id := c.QueryParam("id")
+	idInt, _ := strconv.Atoi(id)
+
+	articles, err := controller.articleUsecase.GetByID(ctx, idInt)
+	if err != nil {
+		return base_response.NewErrorResponse(c, http.StatusBadRequest, err)
+	}
+
+	return base_response.NewSuccessResponse(c, response.FromDomain(articles))
+}
+
+func (controller *ArticleController) Fetch(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	start := c.QueryParam("start")
+	startInt, _ := strconv.Atoi(start)
+	last := c.QueryParam("last")
+	lastInt, _ := strconv.Atoi(last)
+	articles, page, err := controller.articleUsecase.Fetch(ctx, startInt, lastInt)
+	if err != nil {
+		return base_response.NewErrorResponse(c, http.StatusBadRequest, err)
+	}
+
+	return base_response.NewSuccessResponse(c, response.FromListPageDomain(articles, page))
+}
--- controllers/articles/response/response.go
+++ controllers/articles/response/response.go
@@ -1,1 +1,70 @@
 package response
+
+import (
+	"go-schooling/business/articles"
+	"time"
+)
+
+type Articles struct {
+	ID         int       `json:"id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	CategoryID int       `json:"category_id"`
+	ImageID    int       `json:"image_id"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type ArticlesResponse struct {
+	Articles *[]Articles `json:"articles"`
+	Page     int         `json:"page"`
+}
+
+func FromDomain(articleDomain articles.Domain) *Articles {
+	return &Articles{
+		ID:         articleDomain.ID,
+		Title:      articleDomain.Title,
+		Content:    articleDomain.Content,
+		CategoryID: articleDomain.CategoryID,
+		ImageID:    articleDomain.ImageID,
+		CreatedAt:  articleDomain.CreatedAt,
+		UpdatedAt:  articleDomain.UpdatedAt,
+	}
+}
+
+func FromListDomain(articlesDomain []articles.Domain) *[]Articles {
+	articles := []Articles{}
+	for _, value := range articlesDomain {
+		article := Articles{
+			ID:         value.ID,
+			Title:      value.Title,
+			Content:    value.Content,
+			CategoryID: value.CategoryID,
+			ImageID:    value.ImageID,
+			CreatedAt:  value.CreatedAt,
+			UpdatedAt:  value.UpdatedAt,
+		}
+		articles = append(articles, article)
+	}
+	return &articles
+}
+
+func FromListPageDomain(articlesDomain []articles.Domain, Page int) *ArticlesResponse {
+	articles := []Articles{}
+	for _, value := range articlesDomain {
+		article := Articles{
+			ID:         value.ID,
+			Title:      value.Title,
+			Content:    value.Content,
+			CategoryID: value.CategoryID,
+			ImageID:    value.ImageID,
+			CreatedAt:  value.CreatedAt,
+			UpdatedAt:  value.UpdatedAt,
+		}
+		articles = append(articles, article)
+	}
+	articlesResponse := ArticlesResponse{}
+	articlesResponse.Articles = &articles
+	articlesResponse.Page = Page
+	return &articlesResponse
+}
