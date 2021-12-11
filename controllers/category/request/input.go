dce8bf1bca86f3afca3b761c178a96e0b56c686b
Muhammad Reza Bintami <rezabintami@gmail.com>
2021-12-11 23:07:39 +0700

    add request and response category

--- controllers/category/request/input.go
+++ controllers/category/request/input.go
@@ -1,1 +1,15 @@
 package request
+
+import "go-schooling/business/category"
+
+type Category struct {
+	Title  string `json:"title"`
+	Active bool   `json:"status"`
+}
+
+func (req *Category) ToDomain() *category.Domain {
+	return &category.Domain{
+		Title: req.Title,
+		Active: req.Active,
+	}
+}
--- controllers/category/response/response.go
+++ controllers/category/response/response.go
@@ -1,1 +1,24 @@
 package response
+
+import (
+	"go-schooling/business/category"
+	"time"
+)
+
+type Category struct {
+	Id        int       `json:"id"`
+	Title     string    `json:"title"`
+	Active    bool      `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func FromDomain(domain category.Domain) Category {
+	return Category{
+		Id:        domain.ID,
+		Title:     domain.Title,
+		Active:    domain.Active,
+		CreatedAt: domain.CreatedAt,
+		UpdatedAt: domain.UpdatedAt,
+	}
+}
