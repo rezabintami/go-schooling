4b77c8d88d5fbe237cd9403d4490320c26618e23
Muhammad Reza Bintami <rezabintami@gmail.com>
2021-12-22 19:39:56 +0700

    add domain categoryarticles

--- drivers/databases/categoryarticles/record.go
+++ drivers/databases/categoryarticles/record.go
@@ -1,6 +1,15 @@
 package categoryarticles
 
+import "go-schooling/business/categoryarticles"
+
 type CategoryArticles struct {
 	ArticlesID string
 	CategoryID string
+}
+
+func fromDomain(categoryarticlesDomain categoryarticles.Domain) *CategoryArticles {
+	return &CategoryArticles{
+		ArticlesID: categoryarticlesDomain.ArticlesID,	
+		CategoryID: categoryarticlesDomain.CategoryID,
+	}
 }
