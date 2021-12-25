6c74d78260743e883f6b02f6db253b796c0ef1fd
rezabintami <rezabintami@gmail.com>
2021-12-25 21:28:50 +0700

    add delete category and articles by id on pivot categoryarticles

--- business/categoryarticles/domain.go
+++ business/categoryarticles/domain.go
@@ -16,4 +16,6 @@ type Repository interface {
 	GetAllByArticleID(ctx context.Context, id int) ([]Domain, error)
 	// GetByArticleID(ctx context.Context, id int) (Domain, error)
 	GetByCategoryID(ctx context.Context, id int) (Domain, error)
+	DeleteByArticleID(ctx context.Context, id int) error
+	DeleteByCategoryID(ctx context.Context, id int) error
 }
